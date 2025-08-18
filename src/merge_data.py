179c3d8c177a501c5173eac95bf154e84c6cae0a
gmzdag <gamzeddag@hotmail.com>
2025-08-18 19:09:05 +0300

    Merged EV driving data into a unified dataset

---
+++ src/merge_data.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import os
+from pathlib import Path
+
+# Proje yolları
+BASE_DIR = Path(__file__).resolve().parent.parent
+RAW_DIR  = BASE_DIR / "data" / "raw_data"
+OUT_DIR  = BASE_DIR / "data"
+
+all_files = sorted(RAW_DIR.glob("*.csv"))
+
+dfs = [] #merge için df
+
+for file in all_files:
+
+    # Dosya adını al
+    filename = os.path.basename(file).replace(".csv","")
+
+    # Sonda tekrar numarası varsa onu at
+    if filename[-1].isdigit():
+        scenario_name = filename[:-1]
+    else:
+        scenario_name = filename
+
+    # Senaryo bilgisi
+    speed = ''.join([c for c in scenario_name if c.isdigit()]) # 15, 25, 35
+    load = "Loaded" if "L" in scenario_name else "Unloaded"
+    direction = "CISAR" if "C" in scenario_name else "MMF"
+    season = "Summer" if "S" in scenario_name else "Winter"
+
+    df = pd.read_csv(file, sep=",")
+
+    # Eklenecek sütunlar
+    df["speeed_set"] = speed
+    df["load"] = load
+    df["direction"] = direction
+    df["season"] = season
+
+    dfs.append(df)
+
+ev_data = pd.concat(dfs, ignore_index=True)
+
+out_csv = OUT_DIR / "ev_datasetgit .csv"
+ev_data.to_csv(out_csv.as_posix(), index=False)
+
+
+
